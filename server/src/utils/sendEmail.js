8bd715b35a2cad9a253ce51153996d8dd35d43e2
abdul basith <abdulbasithibnhanifa@gmail.com>
2026-02-16 15:21:16 +0530

    fix(server): restore ipv4 force with port 587 to resolve smtp connection

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -4,6 +4,7 @@ const sendEmail = async (to, subject, text) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
+        family: 4, // Force IPv4
         secure: false, // Use STARTTLS
         auth: {
             user: process.env.EMAIL_USER,
