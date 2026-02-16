1647b18b66705c1178ec546a1c4e4234bedc5bca
abdul basith <abdulbasithibnhanifa@gmail.com>
2026-02-16 14:42:48 +0530

    fix(server): force IPv4 for email transport to resolve ENETUNREACH

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -3,9 +3,10 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, text) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
+        family: 4, // Force IPv4
         auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
         },
     });
 
